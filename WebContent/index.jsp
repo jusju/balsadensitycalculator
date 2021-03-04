fb1f65da2557dd1c27af3b82b054b8591b81d1d3
Juslin Jukka <jusju@haaga-helia.fi>
2021-03-04 12:35:22 +0200

    first commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,88 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Balsa Density Calculator</title>
+</head>
+<body>
+<table border="1">
+<form action="Controller">
+  	<th>
+   		<td>
+  		</td>
+  		<td>
+  			Value  
+  		</td>
+  		<td>
+   		</td>
+	</th>
+  	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			Thickness (mm)  
+  		</td>
+  		<td>
+  		  	<input type="number"  name="thickness">
+   		</td>
+	</tr>
+	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			Length (cm)  
+  		</td>
+  		<td>
+  		  	<input type="number"  name="length">
+   		</td>
+	</tr>
+	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			Width (cm)  
+  		</td>
+  		<td>
+  			<input type="number"  name="width">
+   		</td>
+	</tr>	
+	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			Weight  
+  		</td>
+  		<td>
+  			<input type="number"  name="weight">
+   		</td>
+	</tr>	
+	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			Weight  
+  		</td>
+  		<td>
+  			<input type="number"  name="weight">
+   		</td>
+	</tr>		
+	<tr>
+   		<td>
+  		</td>
+  		<td>
+  			  
+  		</td>
+  		<td>
+  			<input type="submit"  name="action" value="calculate">
+   		</td>
+	</tr>	
+
+
+</form>
+</table>
+
+
+</body>
+</html>
---
+++ src/engine/Controller.java
@@ -0,0 +1,80 @@
+package engine;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+import java.time.LocalTime;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if (request.getParameter("action") != null && request.getParameter("action").equals("calculate")) {
+
+			String timeString = LocalTime.now().toString();
+
+			// lähetä merkkijono JSP sivulle attribuuttina
+			request.setAttribute("timeNow", timeString);
+
+			Connection conn = null;
+			try {
+				// db parameters
+				String url = "jdbc:sqlite:balsa.db";
+				// create a connection to the database
+				conn = DriverManager.getConnection(url);
+
+				System.out.println("Yhteys SQL kantaan saatu.");
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				try {
+					if (conn != null) {
+						conn.close();
+					}
+				} catch (SQLException ex) {
+					System.out.println(ex.getMessage());
+				}
+			}
+
+			// forward the request to the index.jsp page
+			request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
