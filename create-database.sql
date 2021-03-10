217ae23d67b7b8ed8f6bc8bca12c462d817a91a8
Juslin Jukka <jusju@haaga-helia.fi>
2021-03-10 14:57:23 +0200

    create table

---
+++ create-database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE balsa (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   tiheys double NOT NULL,
+   korkeus double NOT NULL,
+   leveys double NOT NULL,
+   paino double NOT NULL,
+   pituus double NOT NULL
+);
